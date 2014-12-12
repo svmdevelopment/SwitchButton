4df45b4182b47a320937d3c81454c978b165293c
svmdevelopment <mier9068@gmail.com>
2014-12-12 11:56:11 +0100

    Create switchbutton.js

---
+++ files/switchbutton.js
@@ -0,0 +1,63 @@
+/*/
+ * 
+ * Original by http://cssdeck.com/labs/a-simple-css-switch
+ * Modified and brought to a jQuery Plugin by Stefan van Mierlo
+ * 
+ * MIT License
+ * 
+/*/
+
+(function ($)
+{
+
+    $.fn.SwitchCheckbox = function (options)
+    {
+
+        var settings = $.extend({
+            type: 'round',
+            onLabel: 'ON',
+            offLabel: 'OFF',
+            onComplete: function () { }
+        }, options);
+
+        $(this).filter('input[type="checkbox"]').each(function (e)
+        {
+
+            $(this).css('display', 'none');
+
+            if ($(this).is(':checked'))
+            {
+                label = ['SwitchLabelOff', 'On', 'SwitchLabelOn'];
+            } else
+            {
+                label = ['SwitchLabelOn', 'Off', 'SwitchLabelOff'];
+            }
+
+            if (settings.type == 'round')
+            {
+                $(this).after('<span class="' + label[0] + '">' + settings.onLabel + '</span><div class="Switch Round ' + label[1] + '"><div class="Toggle"></div></div><span class="' + label[2] + '">' + settings.offLabel + '</span>');
+            } else
+            {
+                $(this).after('<div class="Switch ' + label[1] + '"><div class="Toggle"></div><span class="' + label[0] + ' Off">' + settings.onLabel + '</span><span class="' + label[2] + ' On">' + settings.offLabel + '</span></div>');
+            }
+
+        });
+
+        $(document).on('click', '.Switch', function ()
+        {
+            $(this).toggleClass('On').toggleClass('Off');
+            $(this).prev('span').toggleClass('SwitchLabelOn').toggleClass('SwitchLabelOff');
+            $(this).next('span').toggleClass('SwitchLabelOn').toggleClass('SwitchLabelOff');
+
+            SwitchBox = $(this).prevAll('input[type="checkbox"]').filter(':last');
+            SwitchBox.prop('checked', !SwitchBox.prop('checked'));
+
+            if (typeof (settings.onComplete) === "function")
+            {
+                settings.onComplete.call(this);
+            }
+        });
+
+    };
+
+})(jQuery);
